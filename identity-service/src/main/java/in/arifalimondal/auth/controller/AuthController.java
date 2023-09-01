2e0a96ffec93b220f6308708e2dccdf2a4677e07
Arif A Mondal <arifali.mondal@gmail.com>
2023-09-02 00:31:49 +0300

    add token authentication

--- identity-service/src/main/java/in/arifalimondal/auth/controller/AuthController.java
+++ identity-service/src/main/java/in/arifalimondal/auth/controller/AuthController.java
@@ -1,17 +1,44 @@
 package in.arifalimondal.auth.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import in.arifalimondal.auth.dto.AuthRequest;
+import in.arifalimondal.auth.entity.User;
+import in.arifalimondal.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService service;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/register")
+    public String addNewUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @PostMapping("/token")
+    public String getToken(@RequestBody AuthRequest authRequest) {
+        Authentication authenticate =
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        if (authenticate.isAuthenticated()) {
+            return service.generateToken(authRequest.getUsername());
+        } else {
+            throw new RuntimeException("invalid access");
+        }
+    }
+
     @GetMapping("/validate")
     public String validateToken(@RequestParam("token") String token) {
-        //service.validateToken(token);
+        service.validateToken(token);
         return "Token is valid";
     }
 }
