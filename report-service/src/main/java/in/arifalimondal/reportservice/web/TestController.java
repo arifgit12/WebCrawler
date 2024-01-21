49c2aa82f7e4679000af56f142824d1fea406d93
Arif A Mondal <arifali.mondal@gmail.com>
2024-01-22 02:13:16 +0300

    config server init with configuration

--- report-service/src/main/java/in/arifalimondal/reportservice/web/TestController.java
+++ report-service/src/main/java/in/arifalimondal/reportservice/web/TestController.java
@@ -3,6 +3,8 @@ package in.arifalimondal.reportservice.web;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 @Slf4j
+@RefreshScope
 public class TestController {
     private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.name}")
+    private String name;
+
+    @GetMapping("/ping")
+    public String message() {
+        logger.info("Wao!! Application Deployed successfully");
+        return "Wao!! " + name + " Application Deployed successfully...";
+    }
 
     @GetMapping("/t1")
     public String testResource() {
